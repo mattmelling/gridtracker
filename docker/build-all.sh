ad3a5bacf354b721e8fcbeaefe26fd483052878f
Paul Traina <216482-pleasantone@users.noreply.gitlab.com>
2020-11-17 18:29:50 -0800

    Add RPM support to docker build

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -2,8 +2,11 @@
 apt-get -y build-dep .
 dpkg-buildpackage -uc -us
 test -d ../dist/debian || mkdir -p ../dist/debian
+test -d ../dist/rpm || mkdir -p ../dist/rpm
 mv ../*.{deb,dsc,buildinfo,changes,tar.xz} ../dist/debian
 debian/rules clean
 npm install
 npm run dist
 mv package.nw/dist/*.{exe,7z,zip,json} ../dist
+rpmbuild -D "version `node ./version.js`" --build-in-place -bb gridtracker.spec
+mv $HOME/rpmbuild/RPMS/noarch/gridtracker-*.noarch.rpm ../dist/rpm

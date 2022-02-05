cc7742af3df3be5e4d9804484ee9816f08766314
Matthew Chambers <nr0q@gridtracker.org>
2022-02-05 09:54:14 -0600

    rough start for pota stuff

---
+++ package.nw/lib/pota.js
@@ -0,0 +1,22 @@
+// GridTracker Copyright © 2022 GridTracker.org
+// All rights reserved.
+// See LICENSE for more information.
+
+var g_potaPlace = Object;
+var g_potaSpot = Object;
+
+function g_getPotaPlaces()
+{
+  // get list of all parks
+  // we need to see if ever run and update 1x a week
+}
+
+function g_getPotaSpots()
+{
+  // if Pota Spotting enabled, get spots
+}
+
+function g_sendPotaSpot()
+{
+  // if Pota spotting enabled, and we have enough info, send a spot to Pota
+}

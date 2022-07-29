7735a9f97b849c6392f7e113ea3a25cdfab8ac92
csharpen <jonathanf@gmail.com>
2022-07-29 09:06:01 +0000

    If treating RR73/73 as CQ, soften highlighting to help differentiate foreshadow from an actual CQ

--- package.nw/lib/roster/processRosterHunting.js
+++ package.nw/lib/roster/processRosterHunting.js
@@ -786,6 +786,13 @@ function processRosterHunting(callRoster, rosterSettings)
       {
         callingBg = calling + inversionAlpha;
         calling = bold;
+        // If treating RR73/73 as CQ, soften highlighting to help differentiate foreshadow from an actual CQ 
+        if (callObj.DXcall == "RR73" || callObj.DXcall == "73") 
+        {
+          callingConf = `${unconf}#90EE90${inversionAlpha};`;
+          calling = `#90EE90${inversionAlpha};`
+          callingBg = "#000000"
+        }
       }
 
       // Assemble all styles

44d458bc87c9cc390173340a4b0c50c713a848b8
Matthew Chambers <nr0q@gridtracker.org>
2022-10-02 17:16:34 -0500

    lint fix

--- package.nw/lib/gt.js
+++ package.nw/lib/gt.js
@@ -5955,7 +5955,7 @@ function nexradRefresh()
 {
   if (g_Nexrad != null)
   {
-    g_Nexrad.getSource().updateParams({ol3_salt: Math.random()});
+    g_Nexrad.getSource().updateParams({ ol3_salt: Math.random() });
     g_Nexrad.getSource().refresh();
   }
 }
